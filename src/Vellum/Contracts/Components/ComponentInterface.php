c57fdcf034a74336c91766261a5a07ca3eb5d916
Michael Kelly <puritandesigns@gmail.com>
2018-06-06 21:49:24 -0400

    Add getArguments method to ComponentInterface.

--- src/Vellum/Contracts/Components/ComponentInterface.php
+++ src/Vellum/Contracts/Components/ComponentInterface.php
@@ -2,9 +2,12 @@
 
 namespace Vellum\Contracts\Components;
 
+use Vellum\Contracts\Arguments\ArgumentsInterface;
 use Vellum\Contracts\Inputs\InputsInterface;
 
 interface ComponentInterface
 {
     public function getAllInputs(): InputsInterface;
+
+    public function getArguments(): ArgumentsInterface;
 }
